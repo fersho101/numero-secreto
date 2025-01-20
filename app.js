d01f01fe5588ab852c7f455c69e164dc03fda063
fernando corral <146652466+fersho101@users.noreply.github.com>
2025-01-20 09:44:48 -0700

    :art:

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const numMax = 10
+listaNumerosSorteados = []
+numeroSecreto = 0
+intentos = 0
 
 const asignarTextoElemento = (elem, texto) => {
     const elemHTML = document.querySelector(elem)
@@ -5,3 +9,62 @@ const asignarTextoElemento = (elem, texto) => {
     return
 }
 
+const verificarIntento = () => {
+    let numeroUsuario = parseInt(document.getElementById('valorUsuario').value)
+
+    if (numeroUsuario === numeroSecreto) {
+        asignarTextoElemento(
+            'p',
+            `Acertaste el numero en ${intentos} ${
+                intentos === 1 ? 'vez' : 'veces'
+            }`
+        )
+        document.getElementById('reiniciar-btn').removeAttribute('disabled')
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'El numero secreto es menor')
+        } else {
+            asignarTextoElemento('p', 'El numero secreto es mayor')
+        }
+        intentos++
+        limpiarCaja()        
+    }
+}
+
+const limpiarCaja = () => {
+    document.querySelector('input').value = ''
+}
+
+const generarNumeroSecreo = () => {
+    let numeroGenerado = Math.floor(Math.random() * numMax) + 1
+
+    // console.log(numeroGenerado)
+    // console.log(listaNumerosSorteados)
+
+    if (listaNumerosSorteados.length == numMax) {
+        asignarTextoElemento('p', 'Ya se sorteron todos los numeros posibles')
+    } else {
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreo()
+        } else {
+            listaNumerosSorteados.push(numeroGenerado)
+            return numeroGenerado
+        }
+    }
+}
+
+const codicionesIniciales = () => {
+    asignarTextoElemento('h1', 'Juego del numero secreto!')
+    asignarTextoElemento('p', `Digita un numero del 1 al ${numMax}: `)
+    numeroSecreto = generarNumeroSecreo()
+    // console.log(numeroSecreto)
+    intentos = 1
+}
+
+const reiniciarJuego = () => {
+    limpiarCaja()
+    codicionesIniciales()
+    document.getElementById('reiniciar-btn').setAttribute('disabled', 'true')
+}
+
+codicionesIniciales()
